802609d5239173c55b4604b0baf65fd414f34852
bo7193 <bo7193@yandex.ru>
2024-07-26 17:45:36 +0500

    add gui optimization

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from desktop_ui import Ui_MainWindow
+
+class PlayerApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)
+    window = PlayerApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
